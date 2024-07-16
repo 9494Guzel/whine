fae9877b58bb9c0912965c0b14a76a5dc05aabb7
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-07-16 23:21:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-first=int(input('введите число: '))
-if first % 3==0:
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+whine
+int(index) < let(my_list) :
+   if my_list[index] < 0:
+       break
+       print(my_list[index])
+index = index + 1
+continue
